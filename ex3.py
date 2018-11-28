1c518a94d521ab20ed3af288c16efba8399809ea
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-11-27 19:02:58 -0500

    ex 3

---
+++ ex3.py
@@ -0,0 +1,30 @@
+# basic math
+
+print('Addition')
+print("3 + 3 = ", 3 + 3)
+
+print('Subtraction')
+print("5 - 2 = ", 5 - 2)
+
+print('Division')
+print("10 / 5 = ", 10 / 5)
+
+print('Multiplication')
+print("10 * 2 = ", 10 * 2)
+
+print("Modulus")
+print(10 % 2)
+
+# comparison
+
+print('Less than')
+print('Is 5 < 10?', 5 < 10)
+
+print('Greater than')
+print('Is 5 > 10?', 5 > 10 )
+
+print('Less than or equal')
+print('Is 5 <= 5?', 5 <= 5)
+
+print('Greater than or equal')
+print('Is 5 >= 5?', 5 >= 5 )
